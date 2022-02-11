494894599ccf69dfee68a6d9a7b1e1ac4ac4090b
uniqcle <uniqcle@yandex.ru>
2022-02-11 13:01:29 +0700

    mongodb init and connect

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://memo:memo12345@memo.pu8bk.mongodb.net/memo?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+//mongoose.set('useFindAndModify', false); 
