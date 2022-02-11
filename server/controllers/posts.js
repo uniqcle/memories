8303fe37a1c7eb3c1fd364b27adff4af425f261d
uniqcle <uniqcle@yandex.ru>
2022-02-11 14:24:56 +0700

    backend structure

---
+++ server/controllers/posts.js
@@ -0,0 +1,9 @@
+import PostMessage from '../models/postMessage.js'
+
+export const getPosts = (req, res) => {
+    res.send('this works!')
+}
+
+export const createPost = (req, res) => {
+    res.send('Post creation')
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('this works!')
-})
+router.get('/', getPosts)
+router.post('/', createPost)
 
 export default router; 
