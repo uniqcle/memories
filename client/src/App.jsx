45ef3a32b28b84afbad5facd4592001360931bb0
uniqcle <uniqcle@yandex.ru>
2022-02-15 13:39:50 +0700

    styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,17 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from './images/kremlin.png'
+import useStyles from './styles'
 
 const App = () => {
 
+    const classes = useStyles();
+
     return (
         <Container>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memo</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memo</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
